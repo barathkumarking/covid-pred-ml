0551a5c89b2ba4c0baea6b152aaae030231b5571
BARATHKUMAR G <62948487+barathkumarking@users.noreply.github.com>
2021-11-01 20:19:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import pickle
-server = app.server
-app = Flask(__name__)
 
+app = Flask(__name__)
+server = app.server
 model = pickle.load(open('model.pkl', 'rb'))
 
 from app import views
